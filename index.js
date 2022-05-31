e03885b11466f96329a479ba8abf86e47d5d3dac
iMihaiku <icarus81296@gmail.com>
2022-05-31 13:37:09 +0200

    First commit Heroku config

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.post('/api/mensajes', (req, res) => {
   res.json(nuevoMensaje)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`El servidor esta corriendo en el puerto ${PORT}`)
 })
